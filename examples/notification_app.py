8aca33388adc68caac698a1ecb4b8a61dd25a24b
wzcwzc05 <wzcwzc0@outlook.com>
2021-08-07 12:48:19 +0800

    Translation

--- examples/notification_app.py
+++ examples/notification_app.py
@@ -11,6 +11,8 @@
    See the License for the specific language governing permissions and
    limitations under the License.
 """
+"""网页弹出提示的例程
+"""
 
 import remi.gui as gui
 from remi import start, App
@@ -27,26 +29,20 @@ class MyApp(App):
         self.bt = gui.Button('Press me!', width=200, height=30)
         self.bt.style['margin'] = 'auto 50px'
 
-        # setting the listener for the onclick event of the button
         self.bt.onclick.do(self.on_button_pressed)
 
-        # appending a widget to another, the first argument is a string key
         wid.append(self.lbl)
         wid.append(self.bt)
 
-        # returning the root widget
         return wid
 
     # listener function
     def on_button_pressed(self, widget):
-        self.lbl.set_text('A notification message should appear.')
-        self.bt.set_text('Hi!')
-        self.notification_message("Message title", "Hello world!", "")
+        self.lbl.set_text('A notification message should appear.')#修改标签内容
+        self.bt.set_text('Hi!')#修改按钮文本内容
+        self.notification_message("Message title", "Hello world!", "")#弹出提示
 
 
 if __name__ == "__main__":
-    # starts the webserver
-    # optional parameters
-    # start(MyApp,address='127.0.0.1', port=8081, multiple_instance=False,enable_file_cache=True, update_interval=0.1,
-    # start_browser=True)
+    #可选的:start(MyApp,address='127.0.0.1', port=8081, multiple_instance=False,enable_file_cache=True, update_interval=0.1,
     start(MyApp, debug=True, address='0.0.0.0', port=0, )
